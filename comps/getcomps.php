5678361a4e40b72cc435fa4a90600852ec07b8f5
Jeffrey Veenhuis <Jeffroiscool@gmail.com>
2018-12-19 14:52:49 +0100

    Reads out comp directory

---
+++ comps/getcomps.php
@@ -0,0 +1,10 @@
+<?php
+$files = array();
+
+foreach(array_diff(scandir("./"), array('..', '.', "getcomps.php")) as $file){
+	$name = str_replace(".json", "", $file);
+	array_push($files, $name);
+}
+
+echo(json_encode($files));
+?>
